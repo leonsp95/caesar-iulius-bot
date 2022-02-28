b898075fe9b2fb169425e751f83676675142de7a
leonsp95 <rogell@outlook.com.br>
2022-02-28 20:29:28 -0300

    CaesarBot v0.3 release

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from urllib.error import HTTPError
 from urllib.error import URLError
 from bs4 import BeautifulSoup
 import pandas as pd
+import re
 import socket as socket
 import json
 from datetime import datetime
@@ -29,17 +30,18 @@ def welcome(update: Update, context: CallbackContext):
     #START
 def start(update: Update, context: CallbackContext):
     primNome = update.message.from_user.first_name
-    context.bot.send_message(chat_id= update.message.chat_id, text="Olá, " + primNome + "!\nBem-vindo ao <i>CæsarBot 0.2 «Sardegna»</i>! \U0001F916" + "\n----------------------------------------------------\nLISTA DE COMANDOS:\n\U0001F4CB <code>?menu</code> » menu e meus comandos\n\U0001F3A5 <code>?cine</code> » estreias do cinema\n\U0001F3B0 <code>?loto</code> » resultados da loteria\n\U0001F3AE <code>?steam</code> » ofertas do steam\n\u270B\uFE0F <code>?jokenpo</code> » pedra, papel, tesoura\n\U0001F468\u200D\U0001F3EB <code>?udemy</code> » cursos free ou com desconto de 100% na udemy\n\U0001F517 <code>?links</code> » links de compiladores online, bancos de imgs, vídeos, vetores etc.\n\U0001F5D2 <code>?clog</code> » registro de versões e respectivas alterações/correções de erros/melhorias\n----------------------------------------------------\n", parse_mode ='HTML')
+    context.bot.send_message(chat_id= update.message.chat_id, text="Olá, " + primNome + "!\nBem-vindo ao <i>CæsarBot 0.3 «Umbria»</i>! \U0001F916" + "\n----------------------------------------------------\nLISTA DE COMANDOS:\n\U0001F4CB <code>?menu</code> » menu e meus comandos\n\U0001F3A5 <code>?cine</code> » estreias do cinema\n\U0001F3B0 <code>?loto</code> » resultados da loteria\n\U0001F3AE <code>?steam</code> » ofertas do steam\n\u270B\uFE0F <code>?jokenpo</code> » pedra, papel, tesoura\n\U0001F468\u200D\U0001F3EB <code>?udemy</code> » cursos free ou com desconto de 100% na udemy\n\U0001F517 <code>?links</code> » links de compiladores online, bancos de imgs, vídeos, vetores etc.\n\U0001f4d1 <code>?def</code> » retorna o significado de um vocábulo (ex.: <code>?def planta</code>\n\U0001f517 <code>?shrt</code> » encurta links (ex.: <code>?shrt www.google.com</code>\n\U0001f9f2 <code>?mgnt</code> » encurta links magnéticos (ex.: <code>?shrt magnet:?xt=exemplo</code>\n\U0001F5D2 <code>?clog</code> » registro de versões e respectivas alterações/correções de erros/melhorias\n\U0001f50d <code>?info</code> » sobre o bot e seu criador\n----------------------------------------------------\n", parse_mode ='HTML')
     #MENU
 def menu(update: Update, context: CallbackContext):
     primNome = update.message.from_user.first_name
-    context.bot.send_message(chat_id= update.message.chat_id, text="Olá, " + primNome + "!\nBem-vindo ao <i>CæsarBot 0.2 «Sardegna»</i>! \U0001F916" + "\n----------------------------------------------------\nLISTA DE COMANDOS:\n\U0001F4CB <code>?menu</code> » menu e meus comandos\n\U0001F3A5 <code>?cine</code> » estreias do cinema\n\U0001F3B0 <code>?loto</code> » resultados da loteria\n\U0001F3AE <code>?steam</code> » ofertas do steam\n\u270B\uFE0F <code>?jokenpo</code> » pedra, papel, tesoura\n\U0001F468\u200D\U0001F3EB <code>?udemy</code> » cursos free ou com desconto de 100% na udemy\n\U0001F517 <code>?links</code> » links de compiladores online, bancos de imgs, vídeos, vetores etc.\n\U0001F5D2 <code>?clog</code> » registro de versões e respectivas alterações/correções de erros/melhorias\n----------------------------------------------------\n", parse_mode ='HTML')
+    context.bot.send_message(chat_id= update.message.chat_id, text="Olá, " + primNome + "!\nBem-vindo ao <i>CæsarBot 0.3 «Umbria»</i>! \U0001F916" + "\n----------------------------------------------------\nLISTA DE COMANDOS:\n\U0001F4CB <code>?menu</code> » menu e meus comandos\n\U0001F3A5 <code>?cine</code> » estreias do cinema\n\U0001F3B0 <code>?loto</code> » resultados da loteria\n\U0001F3AE <code>?steam</code> » ofertas do steam\n\u270B\uFE0F <code>?jokenpo</code> » pedra, papel, tesoura\n\U0001F468\u200D\U0001F3EB <code>?udemy</code> » cursos free ou com desconto de 100% na udemy\n\U0001F517 <code>?links</code> » links de compiladores online, bancos de imgs, vídeos, vetores etc.\n\U0001f4d1 <code>?def</code> » retorna o significado de um vocábulo (ex.: <code>?def planta</code>\n\U0001f517 <code>?shrt</code> » encurta links (ex.: <code>?shrt www.google.com</code>\n\U0001f9f2 <code>?mgnt</code> » encurta links magnéticos (ex.: <code>?shrt magnet:?xt=exemplo</code>\n\U0001F5D2 <code>?clog</code> » registro de versões e respectivas alterações/correções de erros/melhorias\n\U0001f50d <code>?info</code> » sobre o bot e seu criador\n----------------------------------------------------\n", parse_mode ='HTML')
     #CHANGELOG
 def changelog(update, context):
-    update.message.reply_text("ChangeLog\n0.1.5 'Piemonte'\n\n»Corrigida a localização dos resultados das promoções no Steam\n»Corrigido erro na contagem da pontuação do Jokenpo")
-    update.message.reply_text("ChangeLog\n0.2 «Sardegna» (11.08.2021)\n\n»Adicionado sistema de listas de sites (Fase de teste)\n»Comando ?steam aprimorado:\nI. Modelo de resposta das entradas aprimorado\nII. Corrigido erro de retorno de entradas Steam referentes a pacotes de jogos\nIII. Imagens raspadas diretamente da db de cada jogo, ao invés do link de redirecionamento\nIV. Adicionada a entrada 'Desconto' e link de redirecionamento substituído por hiperlink\n»Nomes de comandos reduzidos para maior facilidade:\nI. ?cinema -> ?cine\nII. ?loteria -> ?loto\nIII. ?changelog -> ?clog\nAdicionadas informações do servidor do bot através do comando ?info")  #INFO
+    update.message.reply_text("ChangeLog\n0.3 «Umbria»\n\n» Adicionado recurso de pesquisa de palavras da língua portuguesa através do comando ?def\n» Adicionado recurso de encurtamento de URL através do comando ?shrt\n» Adicionado recurso de encurtamento de links magnéticos através do comando ?mgnt\n» Alterado o método do recurso Steam (anteriormente os resultados eram retornados através de web scraping, agora, é consumida uma API). Também foi adicionada à entrada a categoria 'Plataforma', que retorna as plataformas em que cada jogo está disponível. Por fim, foram adicionadas categorias de pesquisas (prm, top, nov)\n» Alterada a função ?cine para retornar respostas (entradas de filmes) aleatórios\n» Aprimorada lista de links para materiais, como a seguir:\n    I. Vetores\n    II. Compiladores; etc. \n» Seção ?clog atualizada para retornar apenas o log da última atualização. Os logs das versões anteriores agora estão disponíveis em CaesarBot [Changelog].", parse_mode ='HTML')
+    update.message.reply_text("Logs de versões anteriores: https://telegra.ph/CaesarBot-Changelog-02-28")
+    #INFO
 def myInfo(update, context):
-    context.bot.send_message(chat_id = update.message.chat_id, text= "<b>Bot</b>\nCaesarBot 0.2 «Sardegna»\nCriado por @PrizrakXIII\nImplantado em: 13.05.2021\n\n<b>Servidor</b>\nPlataforma: " + platform.platform() + "\nO.S.: " + platform.system() + " " + platform.machine() +"\nVersão: " + platform.version(), parse_mode="HTML")
+    context.bot.send_message(chat_id = update.message.chat_id, text= "<b>Bot</b>\nCaesarBot 0.3 «Umbria»\nCriado por @LeonParadox\nImplantado em: 13.05.2021\n\n<b>Servidor</b>\nPlataforma: " + platform.platform() + "\nO.S.: " + platform.system() + " " + platform.machine() +"\nVersão: " + platform.version(), parse_mode="HTML")
 #JOKENPO
 def jokenpo(update: Update, context: CallbackContext):
     update.message.reply_text('Vamos jogar <b>Pedra ✊ Papel ✋ Tesoura ✌️</b>?\n<b>REGRAS:</b>\nVocê começa com <b>5</b> vidas e perde uma cada vez que eu ganhar. Se você acertar, ganha <b>1</b> ponto. O jogo acaba quando você não tiver mais vidas restantes.\n<b>COMANDOS:</b>\n<code>pedra</code> / <code>papel</code> / <code>tesoura</code> » jogar\n<code>status</code> » ver pontuação, vidas e empates\n<code>sair</code> » terminar o jogo a qualquer hora\nEscolha uma opção:', parse_mode ='HTML')
@@ -91,6 +93,7 @@ def steamStore(update: Update, context: CallbackContext) -> None:
     context.bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.TYPING)
     res = requests.get(urlLinks.steamAPIUrl)
     paramCom = ['prm', 'nov', 'top', 'brv']
+    nonParam = 'Confira a lista de parâmetros:\n<b>?steam prm</b>: retorna os jogos atualmente em promoção\n<b>?steam nov</b>: retorna os novos jogos\n<b>?steam top</b>: retorna os jogos populares atualmente\n<b>?steam brv</b>: retorna os jogos que serão lançados em breve'
     #### FUNÇÃO
     def listagem(categoria):
         try:
@@ -101,13 +104,8 @@ def steamStore(update: Update, context: CallbackContext) -> None:
                 if varVezes < 5:           
                     gamePic = i['large_capsule_image']
                     gameName = i['name']
-                    if i['original_price'] is None:
-                        gamePriceOrg = "Gratuito"
-                    
-                    
-                    # ADICIONAR VERIFICAÇÃO DE CATEGORIA. SE CATEGORIA FOR COMING_SOON, RETIRAR TODOS OS PRICES
-                    
-                    
+                    if i['original_price'] is None:                        
+                        gamePriceOrg = "Gratuito" 
                     else:
                         gamePriceOrg = (f"R${(i['original_price']/100):.2f}").replace('.',',')
                     if i['discounted'] == True:                           
@@ -116,6 +114,10 @@ def steamStore(update: Update, context: CallbackContext) -> None:
                     else:
                         gamePriceFin = (f"R${(i['final_price']/100):.2f}").replace('.',',')
                         gameDiscount = "Nenhum!"
+                    if categoria == 'coming_soon':
+                            gamePriceOrg = "Indisponível"
+                            gamePriceFin = "Indisponível"
+                            gameDiscount ="Indisponível"
                     if (i['type'] == 0):    tipo = "app/"
                     elif (i['type'] == 1):  tipo = "sub/"
                     elif (i['type'] == 2):  tipo = "bundle/"
@@ -146,9 +148,9 @@ def steamStore(update: Update, context: CallbackContext) -> None:
         elif (context.args[0]) == paramCom[3]:  listagem('coming_soon')
         #elif (context.args[0]) not in paramCom:
         else:
-            update.message.reply_text('Esse parâmetro não existe! Confira a lista de parâmetros:\n <b>?steam ofertas</b>: retorna os jogos atualmente em oferta',parse_mode="HTML")
+            update.message.reply_text(f'Esse parâmetro não existe! {nonParam}',parse_mode="HTML")
     except IndexError:
-        update.message.reply_text('Por favor, envie o comando com um parâmetro!')
+        update.message.reply_text(f'Por favor, envie o comando com um parâmetro! {nonParam}', parse_mode="HTML")
     
 # ------------------------------------------------------------
 
@@ -191,20 +193,16 @@ def udemy(update, context):
     update.message.reply_text('<b>' + str(curso.text)+ '</b>\n' + str(curso.attrs['href']), parse_mode='HTML')
 #LISTA DE SITES
 def listasites(update, context):
-    df = pd.read_csv("lista_links.csv")
-    comp1 = pd.DataFrame(df, columns= [context.args[0]])    
-    comp1 = comp1.dropna()
-    context.bot.send_message(chat_id= update.message.chat_id, text=str(comp1), disable_web_page_preview=True)
+    try:
+        df = pd.read_csv("lista_links.csv")
+        comp1 = pd.DataFrame(df, columns= [context.args[0]])    
+        comp1 = str(comp1.dropna())
+        compFinal = re.sub(r'\d+\s*','', comp1)
+        print(comp1)
+        context.bot.send_message(chat_id= update.message.chat_id, text=compFinal, disable_web_page_preview=True)
+    except:
+        context.bot.send_message(chat_id= update.message.chat_id, text = 'A operação não pôde ser realizada. Adicione um parâmetro ao comando:\n<b>?links parâmetro</b>\nOnde parâmetro pode ser:\ncompiladores\nimagens\nvetores\nvideos\nicones\nfontes', parse_mode='HTML')
 
-#def moedas(update, context):
-#    url = Request("https://www.infomoney.com.br/", headers={'User-Agent': 'Mozilla/5.0'})
-#    html = urlopen(url)
-#    bs = BeautifulSoup(html, 'lxml')
-#    moedinhas = bs.find('div', attrs={'id': 'ticker-content'})
-#    print(bs.text)
-#    listaMoedas = moedinhas.find('a')
-#    print(listaMoedas)
-    
 def vocab(update: Update, context: CallbackContext) -> None:
     try:        
         verbete = requests.get(urlLinks.defUrl + context.args[0])
@@ -237,15 +235,6 @@ def shrt(update: Update, context: CallbackContext) -> None:
         context.bot.send_message(chat_id= update.message.chat_id, text=("Não foi possível encurtar este link. Tente novamente mais tarde."))
         return
 
-def imgA(update: Update, context: CallbackContext):
-    photoFile = update.message.photo[-1].get_file()
-    print(type(photoFile))
-    context.bot.send_message(chat_id = update.message.chat_id, text="Imagem recebida!")
-    photoFile.download('user_pic2.jpg')
-    context.bot.send_photo(chat_id=update.message.chat_id, photo=update.message.photo[-1])
-    #context.bot.send_photo(chat_id=update.message.chat_id, photo=open('user_pic2.jpg', 'rb'))
-   
-
 def mgnt(update: Update, context: CallbackContext) -> None:
     try:
         linkUser = {'m': f'{context.args[0]}'}
@@ -262,8 +251,6 @@ def keyWords(update, context):
     listaPal = ["césar", "cesar", "julio", "júlio", "julius", "kaiser"]
     msgUser = update.message.text.lower()
     updtChat = update.message.chat_id
-    #if (msgUser[::-1] == msgUser) and (len(msgUser) > 3):
-    #    update.message.reply_text('Palíndromo!')
     if "obrigado" in msgUser.lower():
         context.bot.send_video(chat_id = updtChat, video = urlMedia.thx)
     if "cringe" in msgUser:
@@ -288,6 +275,8 @@ def keyWords(update, context):
         context.bot.send_video(chat_id = updtChat, video = urlMedia.freebsd)
     if "ebaaa" in msgUser:
         context.bot.send_video(chat_id = updtChat, video = urlMedia.ebaaa)
+    if "tamo junto" in msgUser:
+        context.bot.send_video(chat_id = updtChat, video = urlMedia.tamojunto)
     for pal in listaPal:
         if pal in msgUser:
             listaResp1 = ["Sim, sou eu mesmo!", "Fala, meu romanizado!", "Veni, vidi, vici!", "Saudações!", "Olá, como tem passado?", "CæsarBot no pedaço!", "Pode falar, companheiro!"]
@@ -299,11 +288,10 @@ def keyWords(update, context):
     if (len(msgUser) > 1) and (msgUser[0] == "?"):
         update.message.reply_text('Este comando <b>não existe</b>!\nPor favor, use <code>?menu</code> para ver os comandos disponíveis.', parse_mode ='HTML')
         return
-#computer = random.choice(listaPal)
-
 
 def main():
-    updater = Updater(token= os.environ['TGTOKEN'])
+    updater = Updater(token= '1834549183:AAH6iOHTSlc-VLfVI9xDFpWu7S2yZZ_fGMQ')
+    #updater = Updater(token= os.environ['TGTOKEN'])
     
     
     dp = updater.dispatcher
@@ -330,10 +318,7 @@ def main():
     dp.add_handler(PrefixHandler('?', 'def', vocab))
     dp.add_handler(PrefixHandler('?', 'shrt', shrt))
     dp.add_handler(PrefixHandler('?', 'mgnt', mgnt))
-    #dp.add_handler(PrefixHandler('?', 'moedas', moedas))
     dp.add_handler(MessageHandler(Filters.text, keyWords))
-    #o handler aguarda texto (e o filtra), e o que estiver dentro de keywords retornará os comandos
-    dp.add_handler(MessageHandler(Filters.photo & Filters.caption_regex(r'\d+'), imgA))
     updater.start_polling()
     updater.idle()
 
